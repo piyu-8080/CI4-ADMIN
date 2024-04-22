6457545582fd8617e71735f0b1ae33f1151f7f78
priyanka <ahirepriya80@gmail.com>
2024-04-22 19:55:18 +0530

    updates

--- app/Views/edit.php
+++ app/Views/edit.php
@@ -53,6 +53,10 @@
                             <label for="email">Email address</label>
                             <input type="email" class="form-control" id="email" name="email" value="<?php echo $user['email']; ?>">
                         </div>
+                        <div class="form-group">
+                            <label for="password">Update Password</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                        </div>
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </form>
                 </div>
